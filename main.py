518ee6ea0864ef8fb18405128b137c3d016d58e7
Harsha <harshahm0130@gmail.com>
2024-07-18 18:08:57 +0530

    Fetching the movie titles

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 from datetime import datetime
+import os
 import requests
 from bs4 import BeautifulSoup
+
+
 
 date = input("Which year do you want to travel to? "
              "Type the date in this format YYYY-MM-DD:")
 
 date_ = datetime.strptime(date, "%d-%m-%Y")
 formatted_date = date_.strftime("%Y-%m-%d")
+print(formatted_date)
 
 URL = f"https://www.billboard.com/charts/hot-100/{formatted_date}/"
 
 response = requests.get(url=URL)
 data = response.text
+
+soup = BeautifulSoup(data, "html.parser")
+
+song_title = soup.select("li ul li h3")
+
+
+song_list = [song.getText().strip() for song in song_title]
+print(song_list)
+
 
 
 
